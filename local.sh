84afc7ccc114e89e0cd0b17b0f4045ac8b89736b
Ed Balduf <balduf@ieee.org>
2014-12-29 12:56:03 -0700

    Initial check-in of working config for my lab.

---
+++ local.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Keep track of the devstack directory
+TOP_DIR=$(cd $(dirname "$0") && pwd)
+
+# Import common functions
+source $TOP_DIR/functions
+
+# Use openrc + stackrc + localrc for settings
+source $TOP_DIR/stackrc
+
+# Destination path for installation ``DEST``
+DEST=${DEST:-/opt/stack}
+
+if is_service_enabled nova; then
+
+    # Get OpenStack admin auth
+    source $TOP_DIR/openrc admin admin
+
+    sudo ovs-vsctl add-port br-ex eth2
+
+    # Get OpenStack demo auth
+    source $TOP_DIR/openrc demo demo
+
+    # Import keys from the current user into the default OpenStack user
+
+    # Add first keypair found in localhost:$HOME/.ssh
+    for i in $HOME/.ssh/id_rsa.pub $HOME/.ssh/id_dsa.pub; do
+        if [[ -r $i ]]; then
+            nova keypair-add --pub_key=$i `hostname`
+            break
+        fi
+    done
+
+    # Add tcp/22 and icmp to default security group
+    nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
+    nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
+
+fi
